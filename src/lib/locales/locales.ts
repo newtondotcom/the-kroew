e6b7ba229b83b242733ac47f659349080608b2ba
newtondotcom <asphalt9fr@gmail.com>
2024-02-16 17:08:12 +0100

    internationalization

---
+++ src/lib/locales/locales.ts
@@ -0,0 +1,16 @@
+const translations = {
+    "en": {
+        "player_number": 'Players number',
+        },
+
+    "fr": {
+        "description1": 'est une application simple créée pour vous aider à vous rappeler où vous avez garé votre voiture. Elle est entièrement open source et gratuite à utiliser. Aucune de vos données n\'est jamais stockée ou utilisée en dehors de votre appareil. Nous respectons votre vie privée et n\'utilisons aucun cookie pour vous suivre.',
+    },
+    };
+
+function translate(key: string) {
+        let locale = navigator.language.split('-')[0] || 'en';
+        return translations[locale][key] || key;
+}
+    
+export default translate;
